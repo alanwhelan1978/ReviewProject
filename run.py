104a77f60603f40b09094ff681661f7f6baadc9a
alanwhelan1978 <alanwhelan1978@gmail.com>
2021-12-29 15:14:16 +0000

    Add to validate_data to stop user putting in value over 5

--- run.py
+++ run.py
@@ -20,7 +20,7 @@ def get_review_data():
     while True:
         print("Please enter your review scores for Service Food Cleanliness and Staff")
         print("Scores should be between 1 and 5. 5 being the best and 1 being the worse")
-        print("Example: 5,4,4,5,3\n")
+        print("Example: 5,4,4,5\n")
 
         data_str = input("Enter your data here: ")
 
@@ -32,25 +32,34 @@ def get_review_data():
 
     return review_data
 
+x = 1
+n = 5
 
 def validate_data(values):
     """
     Inside the try, converts all string values into integers.
     Raises ValueError if strings cannot be converted into int,
-    or if there aren't exactly 6 values.
+    or if there aren't exactly 4 values. Also stops user 
+    adding integers higher than 5
     """
     try:
-        [int(value) for value in values]
+        a = [int(value) for value in values]
         if len(values) != 4:
             raise ValueError(
                 f"Exactly 4 values required, you provided {len(values)}"
             )
+        for value in a:
+            if value not in list(range(1,6)):
+                raise ValueError(
+                    "Score needs to be between 1 and 5"
+                )
+                
     except ValueError as e:
         print(f"Invalid data: {e}, please try again.\n")
+
         return False
-
+    
     return True
-
 
 def update_review_worksheet(data):
     """
